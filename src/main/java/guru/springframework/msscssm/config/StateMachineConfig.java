4a0b626ed64d981bf0b8fd7bf8c6166c0d664794
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-08-26 19:53:12 +0300

    State configuration

---
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -0,0 +1,26 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachineFactory
+@Slf4j
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH_ERROR)
+                .end(PaymentState.AUTH_ERROR);
+    }
+}
